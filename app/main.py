8acc8db3273629419f9eb89ef3a1f8bebf3b8c8e
soringherghisan <sorin.gherghisan@gmail.com>
2024-03-28 19:44:20 +0200

    attempt at passing 2nd stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,14 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket.accept()  # wait for client
+
+    # read data from connection
+    data = server_socket.recv(1000)
+
+    # respond
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    server_socket.send(response)
 
 
 if __name__ == "__main__":
