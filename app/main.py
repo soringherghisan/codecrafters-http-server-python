5fe0027c19c12ed0bad737bdc08e46d64612683e
soringherghisan <sorin.gherghisan@gmail.com>
2024-03-29 18:36:19 +0200

    1st try - respond with 404

--- app/main.py
+++ app/main.py
@@ -17,12 +17,20 @@ def main():
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket, client_addr = server_socket.accept()  # wait for client
+    print(f"Connection from {client_addr}")
 
     # read data from connection
     received_data = client_socket.recv(4096)
+    print(f"Received data {received_data}")
 
-    # respond
-    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    # parse received data & respond accordingly
+    parsed_request = received_data.decode()
+    parsed_request = parsed_request.split("\r\n")
+    parsed_path = parsed_request[0].split()[1]
+    if parsed_path == "/":
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
+    else:
+        response = b"HTTP/1.1 404 Not Found\r\n\r\n"
     client_socket.send(response)
 
 
