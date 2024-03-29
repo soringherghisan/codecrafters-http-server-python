7972866414a2ee2d01c6637c8eff1ce85c4afa04
soringherghisan <sorin.gherghisan@gmail.com>
2024-03-29 17:24:16 +0200

    2nd try

--- app/main.py
+++ app/main.py
@@ -1,3 +1,11 @@
+"""
+socket.accept()
+    Accept a connection. The socket must be bound to an address and listening for connections.
+     The return value is a pair (conn, address) where conn is a new socket object usable to send and receive data
+      on the connection, and address is the address bound to the socket on the other end of the connection.
+
+"""
+
 # Uncomment this to pass the first stage
 import socket
 
@@ -7,16 +15,15 @@ def main():
     print("Logs from your program will appear here!")
 
     # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
+    client_socket, client_addr = server_socket.accept()  # wait for client
 
     # read data from connection
-    data = server_socket.recv(1000)
+    received_data = client_socket.recv(4096)
 
     # respond
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    server_socket.send(response)
+    response = b"HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response)
 
 
 if __name__ == "__main__":
